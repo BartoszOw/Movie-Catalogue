aa99e6533ae60ed104c80487a87d2d33c65b109e
BartoszOw <owbartosz.pl@gmail.com>
2024-03-14 01:06:19 +0100

    main optimalization

--- Movie-Catalogue/main.py
+++ Movie-Catalogue/main.py
@@ -11,36 +11,21 @@ def utility_processor():
     
     return {'tmdb_image_url': tmdb_image_url}
 
+LIST_TYPES = [
+    {'name': "Now Playing", 'type': "now_playing"},
+    {'name': "Top Rated", 'type': "top_rated"},
+    {'name': "Upcoming", 'type': "upcoming"},
+    {'name': "Popular", 'type': "popular"}
+]
+
 @app.route('/')
 def homepage():
-
-    list = [
-        {
-            'name': "Now Playing",
-            'type': "now_playing"
-        },
-        {
-            'name': "Top Rated",
-            'type': "top_rated"
-        },
-        {
-            'name': "Upcoming",
-            'type': "upcoming"
-        },
-        {
-            'name': "Popular",
-            'type': "popular"
-        },
-    ]
-
+    
     selected_list = request.args.get('list_type', 'popular')
-
-    if selected_list not in list:
-        selected_list = 'popular'
 
     movies = tmdb_client.get_movies(8, list_type=selected_list)
 
-    return render_template('homepage.html', movies=movies, list=list, selected_list=selected_list)
+    return render_template('homepage.html', movies=movies, list=LIST_TYPES, selected_list=selected_list)
 
 @app.route('/movie/<movie_id>')
 def movie_details(movie_id):
