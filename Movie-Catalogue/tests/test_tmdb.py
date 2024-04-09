0a699fc87d6b4d1253bad652af51f0dddbdfc3d9
BartoszOw <owbartosz.pl@gmail.com>
2024-04-09 16:38:54 +0200

    Testing

--- Movie-Catalogue/tests/test_tmdb.py
+++ Movie-Catalogue/tests/test_tmdb.py
@@ -1,6 +1,5 @@
 import tmdb_client
-
-
+from unittest.mock import Mock
 
 def test_get_poster_url_uses_default_size():
     poster_api_path = 'some-poster-path'
@@ -14,3 +13,52 @@ def test_get_poster_url_uses_default_size():
 def test_get_movies_list_type_popular():
     movies_list = tmdb_client.get_movies_list(list_type='popular')
     assert movies_list is not None
+
+
+# Mock i monkeypatch
+
+
+def test_get_movies_list(monkeypatch):
+    mock_movies_list = ["Movie 1", "Movie 2"]
+
+    request_mock = Mock()
+    response = request_mock.return_value
+    response.json.return_value = mock_movies_list
+    monkeypatch.setattr('tmdb_client.requests.get', request_mock)
+
+    movies_list = tmdb_client.get_movies_list(list_type='popular')
+    assert movies_list == mock_movies_list
+
+def test_get_single_movie(monkeypatch):
+
+    request_mock = Mock()
+    request_mock.return_value = '333' 
+    monkeypatch.setattr('tmdb_client.get_single_movie', request_mock)
+    res = tmdb_client.get_single_movie()
+    assert res == '333'
+
+def test_get_single_movie_photo(monkeypatch):
+
+    request_mock = Mock()
+    request_mock.return_value = '111'
+    monkeypatch.setattr('tmdb_client.get_single_movie_photo', request_mock)
+    res = tmdb_client.get_single_movie_photo()
+    assert res == '111'
+
+
+def test_get_single_movie_cast(monkeypatch):
+    expected_cast_list = [{'name': "Al Pacino"},{'name': "Steven Bauer"}]
+    request_mock = Mock()
+    request_mock.return_value = expected_cast_list
+    monkeypatch.setattr('tmdb_client.get_single_movie_cast', request_mock)
+    movie_cast = tmdb_client.get_single_movie_cast(movie_id='111')
+    assert movie_cast == expected_cast_list
+
+def test_search(monkeypatch):
+    
+    request_mock = Mock()
+    request_mock.return_value = 'Migration'
+    monkeypatch.setattr('tmdb_client.search', request_mock)
+    res = tmdb_client.search(request_mock.return_value)
+    assert res == 'Migration'
+
